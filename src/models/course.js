7fbf52dffa71a3566ef2f4ac18d9468697d595f2
wiliwomber <johannes.waldorf@outlook.de>
2018-06-13 20:43:55 +0200

    postmaster

--- src/models/course.js
+++ src/models/course.js
@@ -5,7 +5,15 @@ const mongoose = require('mongoose');
 // Define the course schema
 
 const CourseSchema  = new mongoose.Schema({
+    titlelong: {
+        type: String,
+        required: false
+    },
     title: {
+        type: String,
+        required: true
+    },
+    description: {
         type: String,
         required: true
     },
@@ -13,9 +21,81 @@ const CourseSchema  = new mongoose.Schema({
         type: Number,
         required: true
     },
-    description: {
-       type: String,
-       required: true
+    semester: {
+        type: String,
+        required: false
+    },
+    lecturer: {
+        type: String,
+        required: false
+    },
+    chair: {
+        type: String,
+        required: false
+    },
+    registrationstart: {
+        type: Date,
+        required: false
+    },
+    registrationend: {
+        type: Date,
+        required: false
+    },
+    exam: {
+        type: Date,
+        required: false
+    },
+    repeatexamatendofsemester: {
+        type: Boolean,
+        required: false
+    },
+    repeatexamatnextsemester: {
+        type: Boolean,
+        required: false
+    },
+    practicecourse: {
+        type: Boolean,
+        required: false
+    },
+    semesterperiodsperweek: {
+        type: Number,
+        required: false
+    },
+    start: {
+        type: Date,
+        required: false
+    },
+    end: {
+        type: Date,
+        required: false
+    },
+    dow: {
+        type: Number,
+        required: false
+    },
+    roomnumber: {
+        type: Number,
+        required: false
+    },
+    participatorrestriction: {
+        type: Number,
+        required: false
+    },
+    evaluation: {
+        type: Number,
+        required: false
+    },
+    comment: {
+        type: String,
+        required: false
+    },
+    tag: {
+        type: String,
+        required: false
+    },
+    public: {
+        type: Boolean,
+        required: false
     }
 });
 
